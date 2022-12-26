a60e0fb0603344b3d9d2cb5d4db331765bc9c6ea
Crystal Kate <crystalnyan@icloud.com>
2022-12-26 18:54:09 +0500

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,11 @@
 //! Such functionality as transliteration and correction of Uzbek text will
 //! be provided only as an API without sharing code openly later.
 //!
+//! Rewritten from PHP, this Rust crate utilized
+//! regex and pcre crates in order to perform natural
+//! language processing.
+//!
+//! # Upcoming
+//! Public API with transliteration and correction functionality.
 pub mod uzbek;
 pub mod utils;
